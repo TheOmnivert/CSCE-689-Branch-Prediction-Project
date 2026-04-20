84f4550e9751728db5e54cb0122e380537498071
Aditya Viswanadha <adityarao1106@gmail.com>
2026-04-19 21:38:40 -0500

    different hardware budgets for tage

---
+++ branch/seznec_tagesc_192kb/seznec_tagesc_192kb.h
@@ -0,0 +1,32 @@
+#ifndef BRANCH_SEZNEC_TAGESC_192KB_H
+#define BRANCH_SEZNEC_TAGESC_192KB_H
+
+#include <cstdint>
+#include <deque>
+
+#include "address.h"
+#include "modules.h"
+
+class seznec_tagesc_192kb : public champsim::modules::branch_predictor
+{
+public:
+  using branch_predictor::branch_predictor;
+
+  void initialize_branch_predictor();
+  bool predict_branch(champsim::address ip, champsim::address predicted_target, bool always_taken, uint8_t branch_type);
+  void last_branch_result(champsim::address ip, champsim::address branch_target, bool taken, uint8_t branch_type);
+
+private:
+  struct pending_branch {
+    uint64_t seq_no = 0;
+    uint8_t piece = 0;
+    bool prediction = false;
+  };
+
+  static int cbp_branch_type(uint8_t branch_type);
+
+  uint64_t next_seq_no_ = 1;
+  std::deque<pending_branch> pending_conditional_branches_;
+};
+
+#endif
